a72b6eefee8ae822d5397a5aacba938d7a4916ac
Alexander <colddegree@yandex.ru>
2019-06-05 15:18:47 +1000

    Fix typo.

--- random-polygon/script.js
+++ random-polygon/script.js
@@ -2,7 +2,7 @@ function drawPolygon(ctx, points) {
   ctx.beginPath();
   ctx.moveTo(points[0].x, points[0].y);
 
-  for (i = 1; i < points.length; i++) {
+  for (let i = 1; i < points.length; i++) {
     ctx.lineTo(points[i].x, points[i].y);
   }
   ctx.closePath();
